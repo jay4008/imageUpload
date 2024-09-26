14a37ac93edb49170c76799846446ca3ff5f0aed
Jay shah <jay.s@infixzon.com>
2024-09-26 22:27:30 +0530

    new imageshjgjh hjghj hjhgj  jhjkgfdsf

---
+++ ImageFolderResume/header/headerIcon.tsx
@@ -0,0 +1,109 @@
+
+
+const headerIcon = [{
+    note: "Header without profile Image center aligned",
+    img: require('../headerIcon/1.png')
+},
+{
+    note: "Header without profile Image left aligned",
+    img: require('../headerIcon/2.png')
+},
+{
+    note: "Header without profile Image center aligned",
+    img: require('../headerIcon/3.png')
+},
+{
+    note: "Header without profile Image left aligned",
+    img: require('../headerIcon/4.png')
+},
+{
+    note: "Header without profile Image right aligned",
+    img: require('../headerIcon/5.png')
+},
+{
+    note: "Header without profile Image right aligned",
+    img: require('../headerIcon/6.png')
+},
+{
+    note: "Header without profile Image right aligned",
+    img: require('../headerIcon/7.png')
+},
+{
+    note: "Header without profile Image center aligned",
+    img: require('../headerIcon/8.png')
+},
+{
+    note: "Header with profile Image center aligned",
+    img: require('../headerIcon/9.png')
+},
+{
+    note: "Header with profile Image center aligned",
+    img: require('../headerIcon/10.png')
+},
+{
+    note: "Header with profile Image center aligned",
+    img: require('../headerIcon/11.png')
+}, 
+{
+    note: "Header with profile Image center aligned",
+    img: require('../headerIcon/12.png')
+},
+{
+    note: "Header with profile Image center aligned",
+    img: require('../headerIcon/13.png')
+},
+{
+    note: "Header with profile Image center aligned",
+    img: require('../headerIcon/14.png')
+},
+{
+    note: "Header with profile Image center aligned",
+    img: require('../headerIcon/15.png')
+},
+{
+    note: "Modern Header with profile Image ",
+    img: require('../headerIcon/16.png')
+},
+{
+    note: "Modern Header with profile Image",
+    img: require('../headerIcon/17.png')
+},
+{
+    note: "Right aligned theme header with profile Image",
+    img: require('../headerIcon/18.png')
+},
+{
+    note: "Right aligned header with profile Image",
+    img: require('../headerIcon/19.png')
+}, 
+{
+    note: "Left aligned header with profile Image",
+    img: require('../headerIcon/20.png')
+},
+{
+    note: "Left aligned theme header with profile Image",
+    img: require('../headerIcon/21.png')
+},
+{
+    note: "Theme header with profile Image",
+    img: require('../headerIcon/22.png')
+},
+{
+    note: "Modern header with profile Image",
+    img: require('../headerIcon/25.png')
+},
+{
+    note: "Modern header without profile Image",
+    img: require('../headerIcon/24.png')
+},
+{
+    note: "Modern header with profile Image",
+    img: require('../headerIcon/23.png')
+},
+{
+    note: "Header without profile Image",
+    img: require('../headerIcon/26.png')
+},
+];
+
+export { headerIcon};
